aaaf55ecf19760d1d3102f809bf42990b5cced17
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-07 10:22:03 -0800

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i < 8:
-            print(", ", end="")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")

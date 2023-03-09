3368c08c8deb9cee2f2609c1a992568b80c81359
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-09 01:03:00 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-if "__name" == "__main__":
-    from add_0.py import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a,b)))
+from add_0 import add
+
+a = 1
+b = 2
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))

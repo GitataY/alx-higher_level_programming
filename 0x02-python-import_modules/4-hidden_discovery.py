0b85d5c2624281e52e46062549e3809c5e1df315
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-09 03:54:03 -0800

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 from hidden_4 import *
 
 if __name == "__main__":
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+    names = [name for name in dir() if not name.startswith("__")]
+    names.sort()
+    for name in names:
+        print(name)

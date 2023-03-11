d1e08968e4d5fc5fb7795fe612c2535c0139a8a0
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-11 00:18:04 -0800

    largest

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    largest = my_list[0]
+    if my_list == "":
+        return None
+    for num in my_list:
+        if num > largest
+        num = largest
+        return largest

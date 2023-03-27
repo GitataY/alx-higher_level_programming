a7773726c9b3fd52b36d6c1cb4bc8210e2a93b47
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-27 12:11:41 -0700

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        while my_list[count] and count < x:
-            print(my_list[count], end=' ')
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-    except:
-        pass
+        except:
+            pass
     print()
     return count

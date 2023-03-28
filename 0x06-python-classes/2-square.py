808e7b1b4a4e84b0b86cdbd33f810d762ce8720f
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-28 03:12:39 -0700

    type attributes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a square based on 1-square.py"""
+"""This module defines a square based on 1-square.py
+
+   It implements value and type checks for its attributes
+"""
 
 
 class Square:
     """square implementation"""
-     def __init__(self, size=0):
+    def __init__(self, size=0):
+        """Type checks for attributes"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:

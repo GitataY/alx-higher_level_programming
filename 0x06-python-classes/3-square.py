67b8db29151e9ef7941625903ee6d3cfef238080
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-03-28 03:33:30 -0700

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module calculates the area of a square"""
+
+
+classs Square:
+    """This class implements square attributes"""
+    def __init__(self, size=0):
+        """This function specifies conditions"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """This function calculates the area of the square"""
+        self.__size ** 2

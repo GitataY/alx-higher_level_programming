2a5b81929805eaa6195c13b90ea0cfe95813693e
wamuyu <yvonnewamuyug@gmail.com>
2023-06-05 11:22:51 -0700

    set attr

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,5 +28,5 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if value > 0:
+        if value < 0:
             raise ValueError('height must be >=0')

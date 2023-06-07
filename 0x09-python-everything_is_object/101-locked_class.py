59e58f2312366abb441b438f6893c07757bf351a
wamuyu <yvonnewamuyug@gmail.com>
2023-06-07 06:57:42 -0700

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ('first_name',)
 
-    def __setattr__(self, name, value):
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError(f"Cannot create new instance attribute '{name}'")
-        object.__setattr__(self, name, value)
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=''):
+        self.first_name = first_name

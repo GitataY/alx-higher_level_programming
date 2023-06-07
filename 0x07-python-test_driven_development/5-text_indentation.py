44979a7b40521da25a90e1a353ac1fd32d84e679
wamuyu <yvonnewamuyug@gmail.com>
2023-06-07 06:42:19 -0700

    text identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""function that prints a text with 2 new lines after
-    each of these characters: ., ? and :"""
+"""
+
+Module composed by a function that prints 2 new lines after ".?:" characters
+
+"""
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each
-    occurrence of '.', '?', and ':' characters.
+    """ Function that prints 2 new lines after ".?:" characters
 
     Args:
-        text (str): The input text.
+        text: input string
+
+    Returns:
+        No return
 
     Raises:
-        TypeError: If text is not a string.
+        TypeError: If text is not a string
+
 
     """
 
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    punctuation_chars = ['.', '?', ':']
-    output = ""
-    current_line = ""
-    for char in text:
-        current_line += char
-        if char in punctuation_chars:
-            output += current_line.strip() + '\n\n'
-            current_line = ""
-    output += current_line.strip()
-    print(output)
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")

979abf8d4cbb3bb3c3c310965675eaacc5421ea1
wamuyu <yvonnewamuyug@gmail.com>
2023-06-08 11:13:09 -0700

    fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""This class module is a locked class"""
+
+
 class LockedClass:
+    """This class is a locked class"""
 
     __slots__ = ['first_name']
 
     def __init__(self, first_name=''):
+        """function documentation"""
         self.first_name = first_name

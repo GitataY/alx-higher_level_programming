c97b60b00115395585707080b85e57b76c6e7cf5
wamuyu <yvonnewamuyug@gmail.com>
2023-06-13 10:45:01 -0700

    string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module returns an object represented by a JSON strng"""
+
+
+import json
+
+def from_json_string(my_str):
+    """returns an object from json"""
+    return json.loads(my_str)
